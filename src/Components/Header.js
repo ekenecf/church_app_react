feaa1463152c3444d952825ed5eb6652a1512069
ekenecf <ekens2u@gmail.com>
2022-09-10 12:35:26 +0100

    Create mobile component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/style.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import Logo from '../assets/download_st_charles.jpg';
+import MobileMenu from './MobileMenu';
 
-const Header = () => (
-  <header>
-    <nav className="header">
-      <img src={Logo} alt="patron saint" />
-      <ul className="Lists">
-        <li>Home</li>
-        <li>Events</li>
-        <li>Members</li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [toggle, settoggle] = useState(false);
 
+  const handlemenu = () => {
+    settoggle(!toggle);
+    return toggle;
+  };
+
+  return (
+    <header>
+      <nav className="header">
+        <img src={Logo} alt="patron saint" />
+        <ul className="Lists">
+          <li>Home</li>
+          <li>Events</li>
+          <li>Members</li>
+        </ul>
+        <div className="hamburger">
+          <div role="button" tabIndex={0} onClick={handlemenu} onKeyDown={handlemenu}>
+            {
+              toggle ? '' : <GiHamburgerMenu />
+            }
+          </div>
+        </div>
+      </nav>
+      {
+        toggle
+          ? (
+            <div className="MobileContainer">
+              <MobileMenu CancelMenu={handlemenu} />
+            </div>
+          )
+          : null
+      }
+    </header>
+  );
+};
 export default Header;
