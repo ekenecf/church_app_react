27114068e7eb29e0e1746c153335f7e66ae15fb5
ekenecf <ekens2u@gmail.com>
2022-09-10 16:38:32 +0100

    Update mobile menu

--- src/Components/MobileMenu.js
+++ src/Components/MobileMenu.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../css/style.css';
+import { NavLink } from 'react-router-dom';
 
 function MobileMenu(props) {
   const { CancelMenu } = props;
 
   return (
     <div className="mobile">
-      <ul className="Mobile-List">
-        <li>Home</li>
-        <li>Events</li>
-        <li>Members</li>
-      </ul>
+      <div className="Mobile-List">
+        <NavLink to="/">
+          <div role="button" tabIndex={0} onClick={CancelMenu} onKeyDown={CancelMenu}>Home</div>
+        </NavLink>
+        <NavLink to="/Events">
+          <div role="button" tabIndex={0} onClick={CancelMenu} onKeyDown={CancelMenu}>Events</div>
+        </NavLink>
+        <NavLink to="/Members">
+          <div className="link-remove" role="button" tabIndex={0} onClick={CancelMenu} onKeyDown={CancelMenu}>Members</div>
+        </NavLink>
+      </div>
       <div className="cancel" role="button" tabIndex={0} onClick={CancelMenu} onKeyDown={CancelMenu}>X</div>
     </div>
   );
