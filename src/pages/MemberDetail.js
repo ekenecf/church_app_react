5196463e5f16c7529b8de3ab3de424abd01864c1
ekenecf <ekens2u@gmail.com>
2022-10-05 15:51:43 +0100

    Create member detail page

--- src/pages/MemberDetail.js
+++ src/pages/MemberDetail.js
@@ -1,23 +1,28 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { GetMemberDetail } from '../Redux/MemberApi';
 
 import '../css/style.css';
 
 export default function MemberDetail() {
   const { memberDetail } = useSelector((state) => state.MemberDataReducer);
+  const location = useLocation();
+  const { memberid } = location.state || {};
 
-  console.log(memberDetail);
+  console.log(memberid);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(GetMemberDetail());
+    dispatch(GetMemberDetail(memberid));
   }, [dispatch]);
 
   return (
     <div className="MemberDetail">
-      Member MemberDetail
+      {memberDetail.name}
+
+      <button type='button'>Back to Member page</button>
     </div>
   );
 }
--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,1 +1,57 @@
 import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { DeleteMember, GetAllMembers } from '../Redux/MemberApi';
+import { GetUserDetail } from '../Redux/Api';
+import '../css/style.css';
+
+export default function Members() {
+  const dispatch = useDispatch();
+  const { members } = useSelector((state) => state.MemberDataReducer);
+  const { users } = useSelector((state) => state.UserDataReducer);
+
+  console.log(members);
+
+  const adminUser = { ...users[0] };
+  console.log('admin ID', adminUser.id);
+
+  useEffect(() => {
+    dispatch(GetAllMembers());
+    dispatch(GetUserDetail());
+  }, [dispatch]);
+
+  const handleDelete = (userid, groupId, memberid) => {
+    dispatch(DeleteMember(userid, groupId, memberid));
+    window.location.reload();
+  };
+
+  // const handleMemberDetail = (id) => {
+  //   dispatch(GetMemberDetail(id));
+  //   console.log(id);
+  // };
+
+  return (
+    <div className="Members">
+      <h2>All members</h2>
+
+      {
+        members.length
+          ? members.map((member) => (
+            <div key={member.id}>
+              <div>{member.picture}</div>
+              <button
+                type="button"
+                onClick={() => handleDelete(adminUser.id, member.group_id, member.id)}
+              >
+                Delete Member
+              </button>
+              <button type="button">
+                <NavLink to={`/Members/${member.id}`} state={{ memberid: member.id }}>Member detail</NavLink>
+              </button>
+            </div>
+          ))
+          : <div>No Member</div>
+      }
+    </div>
+  );
+}
